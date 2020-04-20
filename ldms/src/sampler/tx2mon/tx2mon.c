6d6e0844e55ddd5f9841a2590150f71efc9049ce
Andy Warner <andyw@pobox.com>
2020-04-19 22:32:04 -0500

    WIP - mostly config method.
    Does not actually add schema yet.

--- ldms/src/sampler/tx2mon/tx2mon.c
+++ ldms/src/sampler/tx2mon/tx2mon.c
@@ -41,11 +41,13 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <sys/errno.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <time.h>
 #include "ldms.h"
 #include "ldmsd.h"
@@ -54,24 +56,15 @@
 
 static ldmsd_msg_log_f msglog;
 static ldms_set_t set = NULL;
-
-#if 0
-/*
- * Forward declarations.
- */
-static void term(struct ldmsd_plugin *self);
-static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl, struct attr_value_list *avl);
-static const char *usage(struct ldmsd_plugin *self);
-static ldms_set_t get_set(struct ldmsd_sampler *self);			/* Obsolete */
-static int sample(struct ldmsd_sampler *self);
-#endif
+#define SAMP "tx2mon"
+static base_data_t base;
 
 /*
  * Plug-in data structure and access method.
  */
 static struct ldmsd_sampler tx2mon_plugin = {
 	.base = {
-		.name = "tx2mon",
+		.name = SAMP,
 		.type = LDMSD_PLUGIN_SAMPLER,
 		.term = term,
 		.config = config,
@@ -90,16 +83,66 @@ struct ldmsd_plugin *get_plugin(ldmsd_msg_log_f pf)
 
 static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl, struct attr_value_list *avl)
 {
-	return -1;
+	int rc = -1;
+	ldms_schema_t schema;
+
+	if (set) {
+		msglog(LDMSD_LERROR, SAMP ": Set already created.\n");
+		return EINVAL;
+	}
+
+	/*
+	 * TODO: parse kwl/avl here
+	 */
+
+	base = base_config(avl, SAMP, SAMP, msglog);
+	if (!base) {
+		/*
+		 * base_del() ?
+		 */
+		return errno;
+	}
+
+	rc = create_metric_set(base);
+	if (rc) {
+		msglog(LDMSD_LERROR, SAMP ": failed to create metric set.\n");
+	}
+
+	return rc;
 }
 
 static const char *usage(struct ldmsd_plugin *self)
 {
-	return "tx2mon: Lorem Ipsum";
+	return SAMP ": Lorem Ipsum";
 }
 
 static void term(struct ldmsd_plugin *self)
 {
+	int i;
+	struct cpu_info *cp;
+
+	for (i = 0; i < tx2mon.n_cpu; i++) {
+		cp = &(tx2mon.cpu_info[i]);
+
+		/*
+		 * None of these test should be required, but ...
+		 */
+		if ((cp->mcp != NULL) && (cp->mcp != MAP_FAILED)) {
+			munmap(cp->mcp);
+			cp->mcp = NULL;
+		}
+
+		if (cp->fd >= 0) {
+			close(cp->fd);
+			cp->fd = -1;
+		}
+	}
+
+	if (base)
+		base_del(base);
+	if (set)
+		ldms_set_delete(set);
+	set = NULL;
 }
 
 static int sample(struct ldmsd_sampler *self)
@@ -114,4 +157,182 @@ static int sample(struct ldmsd_sampler *self)
 static ldms_set_t get_set(struct ldmsd_sampler *self)
 {
 	return set;
+}
+
+/*
+ * Create the schema and metric set.
+ * 
+ *  - Read & parse TX2MON_SOCINFO_PATH to learn about the system config, and
+ *    test that the kernel module is loaded etc. This file is plain ascii.
+ *  - Open & mmap TX2MON_NODE?_PATH files. These are binary data structures
+ *    defined in mc_oper_region.h (that we were magically pointed to during
+ *    configure.)
+ *  - Establish the capabilities reported, based on the version number of
+ *    the data structure.
+ *  - Set up schema, and update housekeeping data.
+ *
+ *    Return 0 iff all good, else report useful error message, clean up,
+ *    and return appropriate errno.
+ */
+static int create_metric_set(base_data_t base)
+{
+	ldms_schema_t schema;
+	int rc = -1;
+	int offset;
+	int cpu;
+	FILE *socinfo;
+	char *path;
+	int i;
+	char buf[TMP_BUF_SIZE];
+	struct cpu_info *cp;
+
+	path = realpath(TX2MON_SOCINFO_PATH, NULL);
+	if (path == NULL) {
+		msglog(LDMSD_LERROR, SAMP ": cannot resolve path for '%s'.\n",
+				TX2MON_SOCINFO_PATH);
+		return EBADF;
+	}
+
+	socinfo = fopen(path, "r");
+	if (!socinfo) {
+		msglog(LDMSD_LERROR, SAMP ": cannot open '%s', %s.\n",
+				path, strerror(errno));
+		free(path);
+		return errno;
+	}
+
+	/*
+	 * Parse socinfo file, it contains three integers with a single
+	 * space between, any problem => fail.
+	 */
+	if (fscanf(socinfo, "%d %d %d", &(tx2mon.n_cpu),
+				&(tx2mon.ncore), &(tx2mon.nthread)) != 3) {
+		msglog(LDMSD_LERROR, SAMP ": cannot parse '%s'.\n", path);
+		fclose(socinfo);
+		free(path);
+		return EBADF;
+	}
+
+	fclose(socinfo);
+	free(path);
+
+	msglog(LDMSD_LINFO, SAMP ": n_cpu: %d, n_core: %d, n_thread: %d.\n",
+			tx2mon.n_cpu, tx2mon.n_core, tx2mon.n_thread);
+
+	if (TX2MON_MAX_CPU < tx2mon.n_cpu) {
+		msglog(LDMSD_LWARN, SAMP ": sampler built for max %d CPUs, system reporting %d CPUs, limiting reporting to %d.\n",
+				TX2MON_MAX_CPU, tx2mon.n_cpu, TX2MON_MAX_CPU);
+		tx2mon.n_cpu = TX2MON_MAX_CPU;
+	}
+
+	if (MAX_CPUS_PER_SOC < tx2mon.n_core) {
+		msglog(LDMSD_LWARN, SAMP ": sampler built for max %d cores, system reporting %d cores, limiting reporting to %d.\n",
+				MAX_CPUS_PER_SOC, tx2mon.n_core, MAX_CPUS_PER_SOC);
+		tx2mon.n_core = MAX_CPUS_PER_SOC;
+	}
+
+	schema = base_schema_new(base);
+	if (!schema) {
+		msglog(LDMSD_LERROR, SAMP ": The schema '%s' could not be created, %s.\n",
+		       base->schema_name, strerror(errno));
+		return errno;
+	}
+
+	tx2mon.cap = CAP_BASIC;
+
+	/*
+	 * Iterate over all CPUs.
+	 */
+	for (i = 0; i < tx2mon.n_cpu; i++) {
+		cp = &(tx2mon.cpu_info[i]);
+
+		cp->mcp = NULL;
+		cp->metric_offset = ldms_schema_metric_count_get(schema);
+
+		/*
+		 * Generate the pathname to the raw file we will open and mmap.
+		 */
+		snprintf(buf, TMP_BUF_SIZE, TX2MON_NODE_PATH, i);
+		path = realpath(buf, NULL);
+
+		if (path == NULL) {
+			msglog(LDMSD_LERROR, SAMP ": cannot resolve path for '%s'.\n",
+					buf);
+			return EBADF;
+		}
+
+		cp->fd = open(path, O_RDONLY);
+		if (cp->fd < 0) {
+			rc = errno;
+			msglog(LDMSD_LERROR, SAMP ": cannot open '%s', %s.\n",
+					path, strerror(rc));
+			free(path);
+			/*
+			 * Free up previously allcoated resources, and bail.
+			 */
+			for (i--; i >= 0; i--) {
+				cp = &(tx2mon.cpu_info[i]);
+				munmap(cp->mcp);
+				close(cp->fd);
+			}
+				
+			return rc;
+		}
+
+		free(path);
+
+		cp->mcp = mmap(NULL, sizeof(struct mc_oper_region), PROT_READ,
+						MAP_SHARED, cp->fd, 0);
+		if (cp->mcp == MAP_FAILED) {
+			rc = errno;
+			close(cp->fd);
+
+			/*
+			 * Free up previously allcoated resources, and bail.
+			 */
+			for (i--; i >= 0; i--) {
+				cp = &(tx2mon.cpu_info[i]);
+				munmap(cp->mcp);
+				close(cp->fd);
+			}
+				
+			return rc;
+		}
+
+		/*
+		 * Check the version of the first structure (they both should
+	:	 * be the same), and map that onto capabilities.
+		 *
+		 * TODO: Check whether we need to wait for CMD_STATUS_READY()
+		 */
+		if (i == 0) {
+			int version = CMD_VERSION(cp->mcp->cmd_status);
+
+			if (i >= 1) {
+				tx2mon.cap |= CAP_THROTTLE;
+			}
+			if (i > 1) {
+				msglog(LDMSD_LWARN, SAMP ": unknown capability: %d. Ignoring.\n", i);
+			}
+		}
+
+		/*
+		 * TODO: build schema here.
+		 */
+	}
+
+	set = base_set_new(base);
+	if (!set) {
+		rc = errno;
+
+		for (i = 0; i < tx2mon.n_cpu; i++) {
+			cp = &(tx2mon.cpu_info[i]);
+
+			munmap(cp->mcp);
+			close(cp->fd);
+		}
+		return rc;
+	}
+
+	return 0;
 }
--- ldms/src/sampler/tx2mon/tx2mon.h
+++ ldms/src/sampler/tx2mon/tx2mon.h
@@ -23,6 +23,15 @@
  */
 
 /*
+ * Header file from github.com/jchandra-cavm/tx2mon.git, which
+ * is required to be checked out elsewhere on the build system,
+ * and pointed to during the configure phase.
+ *
+ * TODO: insert exact configure command line required here.
+ */
+#include "mc_oper_region.h"
+
+/*
  * Forward declarations.
  */
 static void term(struct ldmsd_plugin *self);
@@ -30,3 +39,56 @@ static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl, struct
 static const char *usage(struct ldmsd_plugin *self);
 static ldms_set_t get_set(struct ldmsd_sampler *self);			/* Obsolete */
 static int sample(struct ldmsd_sampler *self);
+
+static int create_metric_set(base_data_t base);
+
+/*
+ * Location of the sysfs entries created by the kernel module.
+ *
+ * NOTE: TX2MON_NODE_PATH is a snprintf() string to create actual path.
+ */
+#define	TX2MON_SYSFS_PATH	"/sys/bus/platform/devices/tx2mon/"
+#define	TX2MON_SOCINFO_PATH	TX2MON_SYSFS_PATH "socinfo"
+#define	TX2MON_NODE_PATH	TX2MON_SYSFS_PATH "node%d_raw"
+
+/*
+ * Max number of CPUs (TX2 chips) supported.
+ */
+#define	TX2MON_MAX_CPU	(2)
+
+/*
+ * Size of temporary buffer used when registering schema etc
+ */
+#define	TMP_BUF_SIZE	(512)
+
+/*
+ * Set of possible capabilities supported by the kernel module.
+ */
+typedef enum {CAP_BASIC = 0x00, CAP_THROTTLE = 0x01} tx2mon_cap;
+	
+/*
+ * Housekeeping structures used internally.
+ */
+
+/*
+ * Per-CPU record keeping
+ */
+struct cpu_info {
+	int	fd,		/* fd of raw file opened */
+	int	metric_offset,	/* starting offset into schema for this CPU */
+	struct	mc_oper_region *mcp,	/* mmapped data structure (from fd) */
+};
+
+/*
+ * Per-sampler record keeping
+ *
+ * TODO: Investigate pulling base & set into here.
+ */
+struct tx2mon_sampler {
+	int	n_cpu,		/* number of CPUs (e.g. TX2 chips) present */
+	int	n_core,		/* cores *per CPU* */
+	int	n_thread,	/* threads *per core* (unused currently) */
+	tx2mon_cap	cap,	/* capabilites of kernel module */
+
+	struct	cpu_info[TX2MON_MAX_CPU];
+} ;
